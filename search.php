681c4399f1592281ca8884b97d2648e129a01f84
Ashley Cameron <ashleycam3ron>
2016-02-25 19:38:23 -0700

    routine push

--- search.php
+++ search.php
@@ -1,45 +1,35 @@
 <?php get_header(); ?>
-	<section id="search" class="container">
-
-		<?php if ( have_posts() ) : ?>
-			<h2><?php printf( __( 'Search Results for: %s', 'elmcreek' ), get_search_query() ); ?></h2>
-
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', 'search' );
-					endwhile;
-
-			// Previous/next page navigation.
-			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'elmcreek' ),
-				'next_text'          => __( 'Next page', 'elmcreek' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'elmcreek' ) . ' </span>',
-			) );
-
-		// If no content, include the "No posts found" template.
-		else : ?>
-			<section class="no-results not-found">
-				<h2><?php _e( 'Nothing Found', 'elmcreek' ); ?></h2>
-				<div class="page-content">
+	<div id="content" class="container wrapper">
+    	<section class="entry col-md-10 col-md-offset-1">
+			<h1><?php printf( __( 'Search Results for: %s', 'ashleycameron' ), get_search_query() ); ?></h1>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <article id="post-<?php the_ID(); ?>">
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <?php the_excerpt("Read More"); ?>
+                    <a href="<?php the_permalink(); ?>">Continue Reading →</a>
+                </article>
+			<?php endwhile; else : ?>
+       		<section class="no-results not-found">
+				<h1><?php _e( 'Nothing posts found.', 'ashleycameron' ); ?></h1>
+				<article>
 					<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
-						<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'elmcreek' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+						<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'ashleycameron' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
 					<?php elseif ( is_search() ) : ?>
 
-						<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'elmcreek' ); ?></p>
+						<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'ashleycameron' ); ?></p>
 						<?php get_search_form(); ?>
 
 					<?php else : ?>
 
-						<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'elmcreek' ); ?></p>
+						<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'ashleycameron' ); ?></p>
 						<?php get_search_form(); ?>
 
 					<?php endif; ?>
 
-				</div><!-- .page-content -->
+				</article><!-- .page-content -->
 			</section><!-- .no-results -->
-
-
-		<?php endif; ?>
-	</section>
-
+	<?php endif; ?>
+    </div>
+</div>
 <?php get_footer(); ?>
